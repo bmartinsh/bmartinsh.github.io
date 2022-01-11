85a2bc213d54ab7d4108b05732d52f9efb72a895
benjamin martin <benjamin@martin-zhu.com>
2022-01-12 00:00:26 +0800

    last change

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ try {
   }
   
   
-  var noteTextarea = $('#text-val');
+  var noteTextarea = $('#note-textarea');
   var instructions = $('#recording-instructions');
   var notesList = $('ul#notes');
   
